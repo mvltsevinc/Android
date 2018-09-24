603e22097e8afca94fc963e421ef4224fa255336
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-09-24 20:30:18 +0300

    Click Demo

---
+++ DemoApp/app/src/main/java/com/mvltsevinc/demoapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mvltsevinc.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+        Log.i("Info","Button Clicked");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
