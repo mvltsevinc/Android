45c59cf5e48393bf249cf27f14811243a4399b6e
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-10-05 23:44:45 +0300

    Layout App

--- FirstApp/app/src/main/java/com/mvltsevinc/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/mvltsevinc/firstapp/MainActivity.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void btnLogClicked(View view) {
         EditText myTextField = (EditText)findViewById(R.id.myTextField);
         EditText pswEditText = findViewById(R.id.pswEditText);
-        Log.i("Info",myTextField.getText().toString() + "\n" + pswEditText.getText().toString());
+
+        Toast.makeText(this, "Welcome " +myTextField.getText().toString(), Toast.LENGTH_SHORT).show();
+
+
+        //Log.i("Info",myTextField.getText().toString() + "\n" + pswEditText.getText().toString());
     }
 
     @Override
