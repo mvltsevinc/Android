669ee37e7c707461f4d1b2ca364cac7bdc570d84
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-10-16 22:58:50 +0300

    Save Adresses to Database

--- TravelBook/app/src/main/java/com/mvltsevinc/travelbook/MainActivity.java
+++ TravelBook/app/src/main/java/com/mvltsevinc/travelbook/MainActivity.java
@@ -1,21 +1,60 @@
 package com.mvltsevinc.travelbook;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
+    static ArrayList<String> addresses = new ArrayList<String>();
+    static ArrayList<LatLng> locations = new ArrayList<LatLng>();
+    static ArrayAdapter arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listview);
+        //DB işlemleri
+        try {
+            MapsActivity.database = this.openOrCreateDatabase("Places", MODE_PRIVATE,null);
+            Cursor cursor = MapsActivity.database.rawQuery("SELECT * FROM places",null);
+            int addressIx = cursor.getColumnIndex("name");
+            int latIx = cursor.getColumnIndex("latitude");
+            int lngIx = cursor.getColumnIndex("longitude");
+            
+            while (cursor.moveToNext()){
+                String address = cursor.getString(addressIx);
+                String latitude = cursor.getString(latIx);
+                String longitude = cursor.getString(lngIx);
+
+                Double lat = Double.parseDouble(latitude);
+                Double lng = Double.parseDouble(longitude);
+                LatLng location = new LatLng(lat,lng);
+
+                addresses.add(address);
+                locations.add(location);
+            }
+            cursor.close();
+
+        }catch (Exception e){
+
+        }
+
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,addresses);
+        listView.setAdapter(arrayAdapter);
     }
 
     /* MENU BAGLAMA ICIN OVERRIDE EDILMESI GEREKEN METHODLAR*/
--- TravelBook/app/src/main/java/com/mvltsevinc/travelbook/MapsActivity.java
+++ TravelBook/app/src/main/java/com/mvltsevinc/travelbook/MapsActivity.java
@@ -4,6 +4,10 @@ import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +16,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,11 +25,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
     LocationManager locationManager;
     LocationListener locationListener;
+    static SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +48,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMapLongClickListener(this);
         locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
             @Override
@@ -116,4 +127,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
     /* Izin verilince calisan method */
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        String address = "";
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
+            if(addressList!=null && addressList.size() > 0) {
+                address += addressList.get(0).getAddressLine(0).toString();
+            }else{
+                address += "New Place";
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mMap.addMarker(new MarkerOptions().title(address).position(latLng));
+        Toast.makeText(getApplicationContext(),"New Place Added!",Toast.LENGTH_LONG).show();
+
+        // Adresi Db ye kaydet
+        try {
+            Double lat = latLng.latitude;
+            Double lng = latLng.longitude;
+            String latStr = lat.toString();
+            String lngStr = lng.toString();
+
+            database = this.openOrCreateDatabase("Places",MODE_PRIVATE,null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS places(name VARCHAR,latitude VARCHAR,longitude VARCHAR)");
+            String sql = "INSERT INTO places(name,latitude,longitude) VALUES (?,?,?)";
+            SQLiteStatement sqLiteStatement = database.compileStatement(sql);
+            sqLiteStatement.bindString(1,address);
+            sqLiteStatement.bindString(2,latStr);
+            sqLiteStatement.bindString(3,lngStr);
+            sqLiteStatement.execute();
+        }catch (Exception e){
+
+        }
+    }
+
 }
