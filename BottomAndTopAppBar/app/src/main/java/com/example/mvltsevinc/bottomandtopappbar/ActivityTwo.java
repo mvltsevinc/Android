265a2b8c13f46b446f09bfeb7b7deb33b54133ab
mvltsevinc <469745mvlt>
2018-12-09 22:00:11 +0300

    Bottom Navigation Bar with Activities

--- BottomAndTopAppBar/app/src/main/java/com/example/mvltsevinc/bottomandtopappbar/ActivityTwo.java
+++ BottomAndTopAppBar/app/src/main/java/com/example/mvltsevinc/bottomandtopappbar/ActivityTwo.java
@@ -3,38 +3,26 @@ package com.example.mvltsevinc.bottomandtopappbar;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private SectionsPagerAdapter sectionsPagerAdapter;
-    private ViewPager viewPager;
+public class ActivityTwo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_two);
 
-        sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        viewPager = findViewById(R.id.container);
-        setupViewPager(viewPager);
-
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
-
-        tabLayout.getTabAt(0).setIcon(R.drawable.ic_assignment);
-        tabLayout.getTabAt(1).setIcon(R.drawable.ic_autorenew);
-        tabLayout.getTabAt(2).setIcon(R.drawable.ic_attach_file);
+        TextView textView = findViewById(R.id.activityTitle2);
+        textView.setText("This is Activity Two");
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavViewBar);
 
         Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(0);
+        MenuItem menuItem = menu.getItem(2);
         menuItem.setChecked(true);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -66,13 +54,5 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-    }
-
-    private void setupViewPager(ViewPager viewPager) {
-         SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
-         adapter.addFragment(new Fragment1());
-         adapter.addFragment(new Fragment2());
-         adapter.addFragment(new Fragment3());
-         viewPager.setAdapter(adapter);
     }
 }
