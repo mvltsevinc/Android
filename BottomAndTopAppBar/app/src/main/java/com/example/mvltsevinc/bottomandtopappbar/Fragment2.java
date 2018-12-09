db0905cd11ac5217dfd9fe6979bc633fdf38fe14
mvltsevinc <469745mvlt>
2018-12-09 22:37:10 +0300

    Adding a CardView to Instagram-like App

--- BottomAndTopAppBar/app/src/main/java/com/example/mvltsevinc/bottomandtopappbar/Fragment2.java
+++ BottomAndTopAppBar/app/src/main/java/com/example/mvltsevinc/bottomandtopappbar/Fragment2.java
@@ -7,12 +7,45 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Fragment2 extends Fragment {
+
+    ListView listView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment2_layout,container,false);
+
+        listView = view.findViewById(R.id.listView);
+
+        ArrayList<Card> cards = new ArrayList<Card>();
+        cards.add(new Card("drawable://" + R.drawable.denmark,"Denmark"));
+        cards.add(new Card("drawable://" + R.drawable.havasu,"Havasu"));
+        cards.add(new Card("drawable://" + R.drawable.denmark,"Denmark"));
+        cards.add(new Card("drawable://" + R.drawable.havasu,"Havasu"));
+        cards.add(new Card("drawable://" + R.drawable.denmark,"Denmark"));
+        cards.add(new Card("drawable://" + R.drawable.havasu,"Havasu"));
+        cards.add(new Card("drawable://" + R.drawable.denmark,"Denmark"));
+        cards.add(new Card("drawable://" + R.drawable.havasu,"Havasu"));
+        cards.add(new Card("drawable://" + R.drawable.denmark,"Denmark"));
+        cards.add(new Card("drawable://" + R.drawable.havasu,"Havasu"));
+        cards.add(new Card("drawable://" + R.drawable.denmark,"Denmark"));
+        cards.add(new Card("drawable://" + R.drawable.havasu,"Havasu"));
+        cards.add(new Card("drawable://" + R.drawable.denmark,"Denmark"));
+        cards.add(new Card("drawable://" + R.drawable.havasu,"Havasu"));
+        cards.add(new Card("drawable://" + R.drawable.denmark,"Denmark"));
+        cards.add(new Card("drawable://" + R.drawable.havasu,"Havasu"));
+        cards.add(new Card("drawable://" + R.drawable.denmark,"Denmark"));
+        cards.add(new Card("drawable://" + R.drawable.havasu,"Havasu"));
+
+        CustomListAdapter adapter = new CustomListAdapter(getActivity(), R.layout.card_layout_main, cards);
+        listView.setAdapter(adapter);
+
+
         return view;
     }
 }
