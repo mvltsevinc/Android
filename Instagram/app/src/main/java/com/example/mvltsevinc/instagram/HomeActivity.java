8b365439cf5d3f9be5166fa1824405ab56e6753d
mvltsevinc <469745mvlt>
2018-12-10 22:32:41 +0300

    Customizing the BottomNavigationView

--- Instagram/app/src/main/java/com/example/mvltsevinc/instagram/HomeActivity.java
+++ Instagram/app/src/main/java/com/example/mvltsevinc/instagram/HomeActivity.java
@@ -3,11 +3,25 @@ package com.example.mvltsevinc.instagram;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mvltsevinc.instagram.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
 public class HomeActivity extends AppCompatActivity {
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
     }
 }
---
+++ Instagram/app/src/main/java/com/example/mvltsevinc/instagram/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,14 @@
+package com.example.mvltsevinc.instagram.Utils;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+    private static final String TAG = "BottomNavigationViewHel";
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+}
