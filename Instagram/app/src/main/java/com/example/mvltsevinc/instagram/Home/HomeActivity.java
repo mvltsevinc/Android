8c6e4ab0170c2342e289d43eaaeb6fa46f771768
mvltsevinc <469745mvlt>
2018-12-11 22:14:15 +0300

    SectionsPagerAdapter Tabs

--- Instagram/app/src/main/java/com/example/mvltsevinc/instagram/Home/HomeActivity.java
+++ Instagram/app/src/main/java/com/example/mvltsevinc/instagram/Home/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.example.mvltsevinc.instagram.Home;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -22,6 +26,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         setupBottomNavigationView();
+        setupViewPager();
     }
 
     /**
@@ -35,5 +40,25 @@ public class HomeActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    /*
+     *   Responsible for adding tabs : Camera,Home,InstagramTv,Messages
+    * */
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment());
+        adapter.addFragment(new HomeFragment());
+        adapter.addFragment(new InstagramTvFragment());
+        adapter.addFragment(new MessagesFragment());
+        ViewPager viewPager = findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_instagram);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_instagram_tv);
+        tabLayout.getTabAt(3).setIcon(R.drawable.ic_arrow);
     }
 }
---
+++ Instagram/app/src/main/java/com/example/mvltsevinc/instagram/Home/SectionsPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.mvltsevinc.instagram.Home;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  Class that stores fragments for tabs
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private static final String TAG = "SectionsPagerAdapter";
+
+    private final List<Fragment> fragmentList = new ArrayList<Fragment>();
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment){
+        fragmentList.add(fragment);
+    }
+}
