1a00e07ed6ee2c8e865e50bc2926e38c0ef7003d
mvltsevinc <469745mvlt>
2018-12-14 22:15:39 +0300

    Building the User Profile Part1

--- Instagram/app/src/main/java/com/example/mvltsevinc/instagram/Profile/ProfileActivity.java
+++ Instagram/app/src/main/java/com/example/mvltsevinc/instagram/Profile/ProfileActivity.java
@@ -23,7 +23,7 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        //setupBottomNavigationView();
+        setupBottomNavigationView();
         setupToolbar();
     }
 
