6273e092d91c54cfafb720b33f86787798f37a2d
mvltsevinc <469745mvlt>
2018-12-16 23:01:37 +0300

    Login Page Layout

--- Instagram/app/src/main/java/com/example/mvltsevinc/instagram/Likes/LikesActivity.java
+++ Instagram/app/src/main/java/com/example/mvltsevinc/instagram/Likes/LikesActivity.java
@@ -18,9 +18,9 @@ public class LikesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_login);
 
-        setupBottomNavigationView();
+        //setupBottomNavigationView();
     }
 
     /**
