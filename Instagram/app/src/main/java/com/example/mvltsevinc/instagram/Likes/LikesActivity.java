3b3de16689d7b99688010ebb84b0bf7e43fdab72
mvltsevinc <469745mvlt>
2018-12-20 21:10:12 +0300

    Register Layout

--- Instagram/app/src/main/java/com/example/mvltsevinc/instagram/Likes/LikesActivity.java
+++ Instagram/app/src/main/java/com/example/mvltsevinc/instagram/Likes/LikesActivity.java
@@ -18,7 +18,7 @@ public class LikesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         //setupBottomNavigationView();
     }
