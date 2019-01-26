5debc38bc63a6379683ea2bf95fdc2dd4f930ec1
mvltsevinc <sevincmevlut@gmail.com>
2019-01-26 18:43:05 +0300

    api configuration and item layout design

---
+++ NewsApp/app/src/main/java/com/example/newsapp/api/ApiInterface.java
@@ -0,0 +1,15 @@
+package com.example.newsapp.api;
+
+import com.example.newsapp.models.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("top-headlines")
+    Call<News> getNews(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+}
