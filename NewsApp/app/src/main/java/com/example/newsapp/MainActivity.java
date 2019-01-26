55e55b07d843772c4208241cebdf5d8b7d9a0cd8
mvltsevinc <sevincmevlut@gmail.com>
2019-01-27 00:07:59 +0300

    inaction search added

--- NewsApp/app/src/main/java/com/example/newsapp/MainActivity.java
+++ NewsApp/app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.newsapp;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.newsapp.api.ApiClient;
 import com.example.newsapp.api.ApiInterface;
 import com.example.newsapp.models.Article;
 import com.example.newsapp.models.News;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,8 +38,8 @@ public class MainActivity extends AppCompatActivity implements  SwipeRefreshLayo
     private RecyclerView.LayoutManager layoutManager;
     private List<Article> articles = new ArrayList<>();
     private Adapter adapter;
+    private TextView topHeadLines;
 
-    private ProgressBar homeProgressBar;
     private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
@@ -43,25 +51,32 @@ public class MainActivity extends AppCompatActivity implements  SwipeRefreshLayo
         swipeRefreshLayout.setOnRefreshListener(this);
         swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
 
+        topHeadLines = findViewById(R.id.topHeadLines);
         recyclerView = findViewById(R.id.recyclerView);
         layoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setNestedScrollingEnabled(false);
-        homeProgressBar = findViewById(R.id.home_loading);
 
-        LoadJson();
+        onLoadingSwipeRefresh("");
     }
 
-    public  void LoadJson(){
+    public  void LoadJson(final String keyword){
 
+        topHeadLines.setVisibility(View.INVISIBLE);
         swipeRefreshLayout.setRefreshing(true);
         ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
 
         String country = Utils.getCountry();
+        String language = Utils.getLanguage();
 
         Call<News> call;
-        call = apiInterface.getNews("bitcoin",API_KEY);
+
+        if(keyword.length() > 0) {
+            call = apiInterface.getNewsSearch(keyword,language,"publishedAt",API_KEY);
+        }else{
+            call = apiInterface.getNews(country,API_KEY);
+        }
 
         call.enqueue(new Callback<News>() {
             @Override
@@ -76,22 +91,66 @@ public class MainActivity extends AppCompatActivity implements  SwipeRefreshLayo
                     adapter = new Adapter(articles,MainActivity.this);
                     recyclerView.setAdapter(adapter);
                     adapter.notifyDataSetChanged();
-                    homeProgressBar.setVisibility(View.GONE);
+
+                    topHeadLines.setVisibility(View.VISIBLE);
                     swipeRefreshLayout.setRefreshing(false);
                 }else{
+                    topHeadLines.setVisibility(View.INVISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
                     Toast.makeText(MainActivity.this, "No Result!", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<News> call, Throwable t) {
-
+                topHeadLines.setVisibility(View.INVISIBLE);
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
     }
 
     @Override
-    public void onRefresh() {
-        LoadJson();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setQueryHint("Search Latest News...");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if(query.length() > 2){
+                    onLoadingSwipeRefresh(query);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchMenuItem.getIcon().setVisible(false,false);
+
+        return true;
     }
+
+    @Override
+    public void onRefresh() {
+        LoadJson("");
+    }
+
+    private void onLoadingSwipeRefresh(final String keyword){
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                LoadJson(keyword);
+            }
+        });
+    }
+
 }
--- NewsApp/app/src/main/java/com/example/newsapp/api/ApiInterface.java
+++ NewsApp/app/src/main/java/com/example/newsapp/api/ApiInterface.java
@@ -9,7 +9,15 @@ import retrofit2.http.Query;
 public interface ApiInterface {
     @GET("top-headlines")
     Call<News> getNews(
-            @Query("q") String query,
+            @Query("country") String country,
             @Query("apiKey") String apiKey
+    );
+
+    @GET("everything")
+    Call<News> getNewsSearch(
+        @Query("q") String keyword,
+        @Query("language") String language,
+        @Query("sortBy") String sortBy,
+        @Query("apiKey") String apiKey
     );
 }
