922761f7791b2ff9e608b6cb6717ab4cf957ff94
mvltsevinc <sevincmevlut@gmail.com>
2019-02-03 21:01:51 +0300

    getting response body

--- Retrofit/app/src/main/java/com/example/retrofit/RedditAPI.java
+++ Retrofit/app/src/main/java/com/example/retrofit/RedditAPI.java
@@ -8,8 +8,6 @@ import retrofit2.http.Headers;
 
 public interface RedditAPI {
 
-    String BASE_URL  = "https://www.reddit.com/";
-
     @Headers("Content-Type: application/json")
     @GET(".json")
     Call<Feed> getData();
