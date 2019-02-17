6b5ef8f8c075e91dc645737241d33a63c9800e8f
mvltsevinc <sevincmevlut@gmail.com>
2019-02-17 23:56:17 +0300

    completed word class

---
+++ SampleNoteApp/app/src/main/java/com/example/samplenoteapp/Word.java
@@ -0,0 +1,53 @@
+package com.example.samplenoteapp;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "id")
+    private Integer mId;
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    @NonNull
+    @ColumnInfo(name = "date")
+    private String mDateTime;
+
+    public Word(@NonNull Integer mId, @NonNull String mWord, @NonNull String mDateTime) {
+        this.mId = mId;
+        this.mWord = mWord;
+        this.mDateTime = mDateTime;
+    }
+
+    public Integer getmId() {
+        return mId;
+    }
+
+    public void setmId(Integer mId) {
+        this.mId = mId;
+    }
+
+    public String getmWord() {
+        return mWord;
+    }
+
+    public void setmWord(String mWord) {
+        this.mWord = mWord;
+    }
+
+    public String getmDateTime() {
+        return mDateTime;
+    }
+
+    public void setmDateTime(String mDateTime) {
+        this.mDateTime = mDateTime;
+    }
+}
