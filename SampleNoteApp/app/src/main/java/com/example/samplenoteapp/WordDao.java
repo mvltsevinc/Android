d1cf3e91ba4d9a2f157f0c19ed1cb02819b166eb
mvltsevinc <sevincmevlut@gmail.com>
2019-02-19 22:27:19 +0300

    Room Database created

--- SampleNoteApp/app/src/main/java/com/example/samplenoteapp/WordDao.java
+++ SampleNoteApp/app/src/main/java/com/example/samplenoteapp/WordDao.java
@@ -1,5 +1,6 @@
 package com.example.samplenoteapp;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
@@ -16,5 +17,5 @@ public interface WordDao {
     void deleteAll();
 
     @Query("SELECT * from word_table ORDER BY word ASC")
-    List<Word> getAllWords();
+    LiveData<List<Word>> getAllWords();
 }
---
+++ SampleNoteApp/app/src/main/java/com/example/samplenoteapp/WordRoomDatabase.java
@@ -0,0 +1,32 @@
+package com.example.samplenoteapp;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
+public abstract  class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+
+    private static WordRoomDatabase INSTANCE;
+
+    public static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    // Create database here
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            // Wipes and rebuilds instead of migrating
+                            // if no Migration object.
+                            // Migration is not part of this practical.
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
