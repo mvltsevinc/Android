b743ab6d050bc0174738f05cdb46b3b443593be4
mvltsevinc <sevincmevlut@gmail.com>
2019-03-03 23:05:14 +0300

    Share Implicit intent

--- SampleNoteApp/app/src/main/java/com/example/samplenoteapp/ui/NewWordActivity.java
+++ SampleNoteApp/app/src/main/java/com/example/samplenoteapp/ui/NewWordActivity.java
@@ -1,12 +1,14 @@
 package com.example.samplenoteapp.ui;
 
 import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.samplenoteapp.R;
 
@@ -15,6 +17,7 @@ public class NewWordActivity extends AppCompatActivity {
             "com.example.samplenoteapp.REPLY";
 
     private EditText mEditWordView;
+    private Button mBtnSave,mBtnShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +25,11 @@ public class NewWordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_word);
 
         mEditWordView = findViewById(R.id.edit_word);
+        mBtnSave = findViewById(R.id.button_save);
+        mBtnShare = findViewById(R.id.button_share);
 
-        final Button button = findViewById(R.id.button_save);
-        button.setOnClickListener(new View.OnClickListener() {
+        // Save Button Click
+        mBtnSave.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent replyIntent = new Intent();
                 if (TextUtils.isEmpty(mEditWordView.getText())) {
@@ -35,6 +40,25 @@ public class NewWordActivity extends AppCompatActivity {
                     setResult(RESULT_OK, replyIntent);
                 }
                 finish();
+            }
+        });
+
+        // Share Button Click
+        mBtnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    if(!TextUtils.isEmpty(mEditWordView.getText().toString())){
+                        String txt = mEditWordView.getText().toString();
+                        String mimeType = "text/plain";
+                        ShareCompat.IntentBuilder
+                                .from(NewWordActivity.this)
+                                .setType(mimeType)
+                                .setChooserTitle(R.string.share_note_with)
+                                .setText(txt)
+                                .startChooser();
+                    }else{
+                        Toast.makeText(NewWordActivity.this, "Please write note", Toast.LENGTH_SHORT).show();
+                    }
             }
         });
     }
